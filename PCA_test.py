2027cb92fef67c134738c90426d6b53ad2643b8e
Snowmwm <snowwm@qq.com>
2017-11-04 23:19:18 +0800

    rename and test data

---
+++ PCA_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pandas import read_csv
+from sklearn.decomposition import PCA
+
+#导入数据
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names = names)
+
+#将数据分为输入数据和输出结果
+array = data.values
+X = array[:, 0:8]
+Y = array[:, 8]
+
+#特征选择
+pca = PCA(n_components = 3)
+fit = pca.fit(X)
+print('解释方差: %s' % fit.explained_variance_ratio_)
+print(fit.components_)
