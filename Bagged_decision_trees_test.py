058572f09e6a626a9a385d318c2f24ad0039babf
Snowmwm <snowwm@qq.com>
2017-11-04 23:24:10 +0800

    数据特征选择

--- Bagged_decision_trees_test.py
+++ Bagged_decision_trees_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#袋装决策树算法
 
 from pandas import read_csv
 from sklearn.ensemble import ExtraTreesClassifier
--- chi_squared_test.py
+++ chi_squared_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#卡方检验
 
 from pandas import read_csv
 from numpy import set_printoptions
