a6eaad60d63ec935818a9ed3377f45b81652f8db
Snowmwm <snowwm@qq.com>
2017-11-06 20:13:46 +0800

    理解元类

--- Metaclass_test.py
+++ Metaclass_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#创建元类
+#创建Say元类
 class SayMetaClass(type):
     def __new__(cls, name, bases, attrs):
         attrs['say_'+name] = lambda self, value, saying = name:print(saying+','+value+'!')
@@ -16,3 +16,22 @@ hello = Hello()
 
 #调用实例方法
 hello.say_Hello('world!')
+
+
+#创建List元类
+class ListMetaClass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['add'] = lambda self, value:self.append(value)
+        return type.__new__(cls, name, bases, attrs)
+        
+#创建类
+class MyList(list, metaclass = ListMetaClass):
+    pass
+    
+#创建实例
+L = MyList()
+
+#调用实例方法
+L.add(1)
+
+print(L)
