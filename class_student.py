9b66dfc837563294517216cbaa5e2441d96450b5
Snowmwm <snowwm@qq.com>
2017-11-10 22:35:54 +0800

    理解类

--- class_student.py
+++ class_student.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 #创建类
-class student(object):
-#通过“__init__”方法绑定属性
-    def __init__(self,name,score):
+class Student(object):
+    def __init__(self, name, score, sex): #通过“__init__”方法绑定属性
+        #把存储在形参中的值传给私有变量
         self.__name = name
         self.__score = score
+        self.__sex = sex
 
     def print_score(self):
         print('%s:%s'%(self.__name,self.__score))
@@ -16,6 +17,12 @@ class student(object):
 
     def get_score(self):
         return self.__score
+        
+    def get_sex(self):
+        if self.__sex == 'M':
+            print('%s is a boy' % self.__name)
+        else:
+            print('%s is a girl' % self.__name)
 
     def set_score(self,score):
         if 0 <= score <= 100:
@@ -30,12 +37,24 @@ class student(object):
             return'B'
         else:
             return'C'
+            
+
+#创建子类            
+class Boy(Student):
+    def __init__(self, name, score, sex='M'):
+        super().__init__(name, score, sex)
+
+class Girl(Student):
+    def __init__(self, name, score, sex='F'):
+        super().__init__(name, score, sex)
 
 #创建实例
-bart = student('Bart Simpson',59)
-lisa = student('Lisa Simpson',87)
+bart = Student('Bart Simpson', 59, 'M')
+lisa = Student('Lisa Simpson', 87, 'F')
+ana = Girl('Ana Conda', 100)
 
 #调用实例方法
+bart.get_sex()
 bart.print_score()
 print(bart.get_grade(90,60))
 bart.set_score(92)
@@ -43,3 +62,7 @@ bart.print_score()
 print(bart.get_grade(90,60))
 lisa.print_score()
 print(lisa.get_grade(90,60))
+ana.get_sex()
+ana.print_score()
+print(ana.get_grade(90,60))
+
